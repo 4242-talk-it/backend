288656e3f69f30a89991a4fa8182f6de61f19fbe
chlqhrud <kelly07192@gmail.com>
2026-02-25 16:15:26 +0900

    [ feat ] : 북마크 토글 및 상태 조회 기능 구현

--- src/main/java/com/talkit/app/domain/community/entity/Community.java
+++ src/main/java/com/talkit/app/domain/community/entity/Community.java
@@ -48,6 +48,9 @@ public class Community extends BaseEntity {
     @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CommunityLike> communityLikeList = new ArrayList<>();
 
+    @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Bookmark> bookmarkList = new ArrayList<>();
+
     @ElementCollection(fetch = FetchType.LAZY)
     @CollectionTable(name = "community_tags", joinColumns = @JoinColumn(name = "community_id"))
     @Column(name = "tag_name")
